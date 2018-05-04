8549902fd1177be8b7a8d3751203818b0724fa75
SangWoo <sangw0804@likelion.org>
2018-05-04 23:09:52 +0900

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get '/signup' => 'users#new'
 
-  get 'static_pages/help'
+  get '/help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact' => 'static_pages#contact'
 
   root 'static_pages#home'
 
