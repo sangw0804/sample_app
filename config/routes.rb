4f37e835d0b68675cad42f20f09e87b21f601491
SangWoo <sangw0804@likelion.org>
2018-05-07 18:29:23 +0900

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
 
+  post '/signup' => 'users#create'
+
   get '/help' => 'static_pages#help'
 
   get '/about' => 'static_pages#about'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  resources :users
 end
